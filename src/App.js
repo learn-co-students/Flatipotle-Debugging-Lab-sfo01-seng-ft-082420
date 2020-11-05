8e479997791f714b455bb0f9d130f4cc953573cc
mehmehmehlol <hkmeganlo@gmail.com>
2020-11-04 22:39:09 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import './App.css'
-import Form from './Form'
-import Order from './Order'
+import Form from './components/Form'
+import Order from './components/Order'
 
 class App extends Component {
-  state: {
+  state = {
     orders: []
   }
 
@@ -16,8 +16,9 @@ class App extends Component {
 
   render() {
     const orders = this.state.orders.map( (order, idx) => {
-      <Order key={idx} {...order} />
+        return <Order key={idx} {...order} />
     })
+    
 
     return (
       <div className="App">
